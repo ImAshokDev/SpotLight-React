5e253b9a2a3ac69fc7f2cb64de7d773fef1cbca8
krist yohan <massashok420@gmail.com>
2019-10-04 17:19:35 +0530

    done with content section

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.scss';
 import Header from './header/Header';
 import img from './header/01.jpg';
+import Content from './content/Content';
 
 const App = () => (
   <div className="spotlight">
     <Header bg={img} />
+    <Content />
   </div>
 );
 
---
+++ src/content/Content.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Content.scss';
+
+const Content = () => (
+  <div className="content-section">
+    <div>
+      <h6>Aromatic Beauty Saloon and Spa!</h6>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu libero
+        scelerisque ligula sagittis faucibus eget quis lacus.Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit.
+      </p>
+    </div>
+  </div>
+);
+
+export default Content;
