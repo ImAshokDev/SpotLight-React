968fa2ae232af6afe4a4e2116e4a8f1ac05a5a3c
krist yohan <massashok420@gmail.com>
2019-10-04 13:37:29 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SpotLight site in React
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div className="spotlight">
+    <p>1</p>
+  </div>
+);
+
+export default App;
