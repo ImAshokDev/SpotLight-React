fdc13d3e7b5a33fd4c7b9b1f5f9c4bea17722a18
krist yohan <massashok420@gmail.com>
2019-10-05 15:21:10 +0530

    Done with details section

---
+++ src/details/Details.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Details.scss';
+
+const Details = () => (
+  <div className="details-section">
+    <div>
+      <p>
+        Consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Praesent eget risus vitae massa semper aliquam quis
+        mattis consectetur adipiscing elit..
+      </p>
+      <h6>markus denny</h6>
+    </div>
+  </div>
+);
+
+export default Details;
